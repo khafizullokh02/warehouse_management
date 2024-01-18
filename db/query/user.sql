99c7508bdf5ee170b5791e23e66c70922ba2c440
khafizullokh02 <khafizullokhulmasov@gmail.com>
2024-01-18 21:59:58 +0500

    fix/add: middleware for api was added

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,10 +1,12 @@
 -- name: CreateUser :one
 INSERT INTO users (
   name,
+  role,
   email,
   password
 ) VALUES (
   sqlc.arg(name),
+  sqlc.arg(role),
   sqlc.arg(email),
   sqlc.arg(password)
 ) RETURNING *;
@@ -26,12 +28,7 @@ OFFSET $3;
 -- name: UpdateUser :one
 UPDATE users
 SET
-name = COALESCE(sqlc.arg(name), name),
-email = COALESCE(sqlc.arg(email), email),
-password = COALESCE(sqlc.arg(password), password),
-created_at = COALESCE(sqlc.arg(created_at), created_at),
-updated_at = COALESCE(sqlc.arg(updated_at), updated_at),
-deleted_at = COALESCE(sqlc.arg(deleted_at), deleted_at)
+password = COALESCE(sqlc.arg(password), password)
 WHERE id = sqlc.arg(id)
 RETURNING *;
 
